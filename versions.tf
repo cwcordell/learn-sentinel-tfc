d7cfbe2798381ebaaa13b1f6cd0db0cc12fe622e
Cory W. Cordell <cory.cordell@myemail.com>
2022-09-29 13:52:11 -0500

    changed org name

--- versions.tf
+++ versions.tf
@@ -13,7 +13,7 @@ terraform {
   required_version = ">= 0.15"
 
   backend "remote" {
-    organization = "Cordell Tech"
+    organization = "cordell-tech"
 
     workspaces {
       name = "sentinel-example"
